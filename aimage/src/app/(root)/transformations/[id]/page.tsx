b06a882d40d3b4ef0d8277492710be1017e49a3c
manishkumar632 <manishmu632@gmail.com>
2024-06-13 21:53:10 +0530

    routes created

---
+++ aimage/src/app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransformationsPage = ({params}: {params: {id: string}}) => {
+    return <div>TransformationsPage</div>;
+};
+
+export default TransformationsPage;
