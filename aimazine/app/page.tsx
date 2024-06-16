d6cee2435dbcfe7e7ab2367a3eb8de95b39e9f45
manishkumar632 <manishmu632@gmail.com>
2024-06-16 13:55:29 +0530

    updated - npx shadcn-ui@latest add button

--- aimazine/app/page.tsx
+++ aimazine/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Hello AI SAAS</h1>
+        <div className="text-3xl font-bold">
+            <h1 className="text-green-500">Hello AI SAAS</h1>
         </div>
     );
 }
