bd3fa339ec5a735852fa7e4ec1524be851235d32
manishkumar632 <manishmu632@gmail.com>
2024-06-27 14:56:10 +0530

    updated

--- aimazine/app/(auth)/layout.tsx
+++
@@ -1,8 +0,0 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className="flex h-screen w-screen items-center justify-center">
-            {children}
-        </div>
-    )
-}
-export default AuthLayout
